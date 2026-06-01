d852969624438bae6bbe93b6e70d3edce9816501
Redoan Khandoker <redoankhandoker8066@gmail.com>
2026-06-01 13:10:04 +0600

    red

---
+++ Growing_Sequences.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+int x;
+while (x--)
+{
+cin>>x; 
+ for (int i = 1; i <= x; i++)
+ {
+     cout<<i<<" "; 
+ }
+ cout<<endl; 
+ 
+}
+
+
+
+
+
+
+return 0;
+}
